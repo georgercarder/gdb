9e5405bcc512379757e8ea8741d38f674368b0c4
George Carder <georgercarder@gmail.com>
2020-07-22 20:17:50 -0700

    license gpl v3

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+# Copyright (c) 2020 George Carder georgercarder@gmail.com
+# GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+# see LICENSE at project root
--- scripts/gdp.sh
+++ scripts/gdp.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Copyright (c) 2020 George Carder georgercarder@gmail.com
+# GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+# see LICENSE at project root
+
 echo "TODO"
 
 # check defaultToExecutable for config

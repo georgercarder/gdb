125d068bffdfbf6b7aa7a4c37d8842c39b7ab93b
George Carder <georgercarder@gmail.com>
2020-07-22 20:14:34 -0700

    outlining

--- install.sh
+++ install.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-echo "TODO"
+# outlining
+
+# check for ssh installed
+# example
+# ssh -V
+# echo $?  (0 if installed, else nonzero)
+
+# check for rsync installed
+# rsync -V
+# echo $? (0 if installed, else nonzero)
+
+# then do same as gfmt
+ln scripts/gdp.sh /usr/bin/gdp
+errFlag=$(($errFlag||$?))
+if [ ! $errFlag -eq 0 ]; then
+	echo ""
+	echo "installation unsuccessful."
+	#echo "TODO SUGGESTIONS"
+	exit 1
+fi
+echo "installation complete."
+echo ""
+echo "use: run 'gdp' for instructions."
---
+++ scripts/help.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+# basic instructions
+
+# advanced instructions

0efb9dd7c423c9790cc15fd9f85157af07e8f099
George Carder <georgercarder@gmail.com>
2020-07-22 19:33:40 -0700

    outlining basic functionality

--- scripts/gdp.sh
+++ scripts/gdp.sh
@@ -1,3 +1,47 @@
 #!/bin/bash
 
 echo "TODO"
+
+# check defaultToExecutable for config
+#  if none exists, populate and save config
+
+####################################
+### configuration ##################
+####################################
+#
+# projectname/alias
+#
+# project root (fs path)
+#
+# hosts <--> privKeys
+#
+# git repository 
+#
+####################################
+
+####################################
+### control ########################
+####################################
+#
+# start
+#
+# stop
+#
+# restart
+#
+####################################
+
+####################################
+## deployment ######################
+####################################
+#
+# options
+#   (default start after build)?
+#
+# dev mode
+#   cp diff to nodes, run build
+#
+# prod mode
+#   run git pull on node, run build
+#
+####################################

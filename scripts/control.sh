347590dd1b6c57a9779f854be4f82dfa7b198c42
George Carder <georgercarder@gmail.com>
2020-07-24 13:23:28 -0700

    outlining control

--- scripts/control.sh
+++ scripts/control.sh
@@ -4,5 +4,63 @@
 # GENERAL PUBLIC LICENSE Version 3, 29 June 2007
 # see LICENSE at project root
 
-echo "control is currently in development"
-#TODO
+pwd_=$(pwd)
+gdpLogs=$pwd_/.gdp/gdp.logs
+gdpConfig=$pwd_/.gdp/gdp.config
+flags=$1
+####################################
+### control ########################
+####################################
+#
+# start s 
+#
+# restart r
+#
+# stop t (terminate) 
+#
+# switch w 
+#   (for advanced CI)
+#
+####################################
+flag_search() {
+	flags=$1
+	token=$2
+	found=$(echo $flags | grep $token)
+	if [ ! -z "$found" ]; then
+		echo 1
+	else
+		echo 0
+	fi
+}
+s=$(flag_search $flags s)
+r=$(flag_search $flags r)
+t=$(flag_search $flags t)
+w=$(flag_search $flags w)
+#start
+if [ $s -eq 1 ]; then
+	#TODO start
+	echo "start"
+	exit 0 
+	#to prevent from doing excessive control calls below
+fi
+#restart
+if [ $r -eq 1 ]; then
+	#TODO restart
+	echo "restart"
+	exit 0
+	#to prevent from doing excessive control calls below
+fi
+#stop
+if [ $t -eq 1 ]; then
+	#TODO stop	
+	echo "stop"
+	exit 0
+	#to prevent from doing excessive control calls below
+fi	
+#switch
+if [ $w -eq 1 ]; then
+	#TODO switch
+	echo "switch"
+	exit 0
+fi
+
